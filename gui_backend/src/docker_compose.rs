b6249c7465cbdbcc77595c1b74ee6202ca1899a3
yohan.velay <yohan.velay@andil.fr>
2024-06-28 11:09:53 +0200

    new env handling + rcon connection

--- gui_backend/src/docker_compose.rs
+++ gui_backend/src/docker_compose.rs
@@ -1,8 +1,9 @@
 use serde::{Deserialize, Serialize};
-use serde_yaml::{from_reader, to_writer, Value};
+use serde_yaml::{from_reader, to_writer, Value, Mapping};
 use std::fs::File;
 use std::path::{Path, PathBuf};
 use std::process::Command;
+use std::error::Error;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct DockerCompose {
@@ -12,7 +13,7 @@ pub struct DockerCompose {
 
 impl DockerCompose {
     // Constructor to create a new DockerCompose instance from a file path
-    pub fn new<P: AsRef<Path>>(path: P) -> Result<Self, Box<dyn std::error::Error>> {
+    pub fn new<P: AsRef<Path>>(path: P) -> Result<Self, Box<dyn Error>> {
         let file = File::open(&path)?;
         let docker_compose: serde_yaml::Value = from_reader(file)?;
 
@@ -49,7 +50,7 @@ impl DockerCompose {
     }
 
     // Method to set a value in a specific service
-    pub fn set_value(&mut self, service_name: &str, key: &str, value: serde_json::Value) -> Result<(), Box<dyn std::error::Error>> {
+    pub fn set_value(&mut self, service_name: &str, key: &str, value: serde_json::Value) -> Result<(), Box<dyn Error>> {
         let yaml_value: Value = serde_json::from_value(value)?;
         if let Some(service) = self.content.get_mut("services")
             .and_then(Value::as_mapping_mut)
@@ -73,7 +74,7 @@ impl DockerCompose {
     }
 
     // Method to remove a value from a specific service
-    pub fn remove_value(&mut self, service_name: &str, key: &str) -> Result<(), Box<dyn std::error::Error>> {
+    pub fn remove_value(&mut self, service_name: &str, key: &str) -> Result<(), Box<dyn Error>> {
         if let Some(service) = self.content.get_mut("services")
             .and_then(Value::as_mapping_mut)
             .and_then(|services| services.get_mut(&Value::String(service_name.to_string())))
@@ -86,15 +87,65 @@ impl DockerCompose {
         }
     }
 
+        // Method to set an environment variable in a specific service
+        pub fn set_env(&mut self, service_name: &str, key: &str, value: &str) -> Result<(), Box<dyn Error>> {
+            let yaml_value = Value::String(value.to_string());
+            if let Some(service) = self.content.get_mut("services")
+                .and_then(Value::as_mapping_mut)
+                .and_then(|services| services.get_mut(&Value::String(service_name.to_string())))
+                .and_then(Value::as_mapping_mut)
+            {
+                if let Some(env) = service.get_mut("environment").and_then(Value::as_mapping_mut) {
+                    env.insert(Value::String(key.to_string()), yaml_value);
+                } else {
+                    let mut env = Mapping::new();
+                    env.insert(Value::String(key.to_string()), yaml_value);
+                    service.insert(Value::String("environment".to_string()), Value::Mapping(env));
+                }
+                Ok(())
+            } else {
+                Err("Service not found".into())
+            }
+        }
+    
+        // Method to get an environment variable from a specific service
+        pub fn get_env(&self, service_name: &str, key: &str) -> Option<&Value> {
+            self.content.get("services")
+                .and_then(Value::as_mapping)
+                .and_then(|services| services.get(&Value::String(service_name.to_string())))
+                .and_then(Value::as_mapping)
+                .and_then(|service| service.get("environment"))
+                .and_then(Value::as_mapping)
+                .and_then(|env| env.get(&Value::String(key.to_string())))
+        }
+    
+        // Method to remove an environment variable from a specific service
+        pub fn remove_env(&mut self, service_name: &str, key: &str) -> Result<(), Box<dyn Error>> {
+            if let Some(service) = self.content.get_mut("services")
+                .and_then(Value::as_mapping_mut)
+                .and_then(|services| services.get_mut(&Value::String(service_name.to_string())))
+                .and_then(Value::as_mapping_mut)
+            {
+                if let Some(env) = service.get_mut("environment").and_then(Value::as_mapping_mut) {
+                    env.remove(&Value::String(key.to_string()));
+                    Ok(())
+                } else {
+                    Err("Environment section not found".into())
+                }
+            } else {
+                Err("Service not found".into())
+            }
+        }
+
     // Method to save the current Docker Compose configuration back to a file
-    pub fn save(&self) -> Result<(), Box<dyn std::error::Error>> {
+    pub fn save(&self) -> Result<(), Box<dyn Error>> {
         let file = File::create(&self.path)?;
         to_writer(file, &self.content)?;
         Ok(())
     }
 
     // Method to start the docker
-    pub fn start(&self) -> Result<(), Box<dyn std::error::Error>> {
+    pub fn start(&self) -> Result<(), Box<dyn Error>> {
         let status = Command::new("docker")
             .arg("compose")
             .arg("up")
--- gui_backend/src/main.rs
+++ gui_backend/src/main.rs
@@ -2,55 +2,108 @@ mod docker_compose;
 mod server;
 
 use serde_json;
+use rand::Rng;
+use minecraft_client_rs::Client;
+use std::time::Duration;
+use std::thread;
 
 use docker_compose::DockerCompose;
 use server::start_server;
+
+fn generate_password(length: usize) -> String {
+    let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#@";
+
+    let mut rng = rand::thread_rng();
+    let random_string: String = (0..length)
+        .map(|_| {
+            let idx = rng.gen_range(0..chars.len());
+            chars.chars().nth(idx).unwrap()
+        })
+        .collect();
+
+    random_string
+}
+
+async fn connect_rcon(password: String) -> Client {
+    let mut client;
+    loop {
+        client = Client::new("127.0.0.1:25575".to_string()).unwrap();
+        match client.authenticate(password.to_string()) {
+            Ok(_) => { 
+                println!("Authenticated"); 
+                break;
+            },
+            Err(err) => { println!("Authentication Error.\nErr: {:?}", err); },
+        }
+        thread::sleep(Duration::from_secs(10));
+    }
+    client
+}
 
 #[actix_web::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let path = "docker-compose.yml";
     let mut docker_compose = DockerCompose::new(path)?;
+    let rcon_password = generate_password(16);
 
     // Adding a new service
-    let new_service = serde_json::json!({
-        "image": "nginx:latest",
-        "ports": ["80:80"]
+    let mc_service = serde_json::json!({
+        "image": "itzg/minecraft-server",
+        "tty": true,
+        "stdin_open": true,
+        "volumes": ["./data:/data"],
+        "environment": serde_json::json!({
+            "EULA": "true",
+            "TYPE": "VANILLA",
+            "VERSION": "LATEST",
+            "MEMORY": "1G",
+            "LOG_TIMESTAMP": "true", 
+        })
     });
-    docker_compose.set_service("nginx", new_service);
-
-    // Removing an existing service
-    docker_compose.remove_service("nginx");
+    docker_compose.set_service("mc", mc_service);
+    docker_compose.set_value("mc", "ports", serde_json::json!(["25565:25565","25575:25575"]))?;
+    docker_compose.set_env("mc", "RCON_PASSWORD", &rcon_password)?;
 
     // Getting a value from the nginx service
-    if let Some(value) = docker_compose.get_value("mc", "image") {
-        println!("Image: {:?}", value);
-    }
-    
-    // Removing a value from the nginx service
-    docker_compose.remove_value("mc", "ports")?;
+    // if let Some(value) = docker_compose.get_value("mc", "environment") {
+    //     println!("Env: {:#?}", value);
+    // }
 
     // Getting a service
-    if let Some(service) = docker_compose.get_service("mc") {
-        println!("Service found: {:?}", service);
-    } else {
-        println!("Service not found");
-    }
-
-    docker_compose.set_value("mc", "ports", serde_json::json!(["25565:25565"]))?;
+    // if let Some(service) = docker_compose.get_service("mc") {
+    //     println!("Service found: {:#?}", service);
+    // } else {
+    //     println!("Service not found");
+    // }
 
     // Saving changes back to the file
     docker_compose.save()?;
     docker_compose.start()?;
 
+    // Create a new client and connect to the server.
+    let mut client = connect_rcon(rcon_password).await;
+
+    match client.send_command("seed".to_string()) {
+        Ok(resp) => { println!("{}", resp.body); }, // "Seed: [1871644822592853811]"
+        Err(err) => { println!("Command Error.\nErr: {:?}", err);},
+    }
+
+    match client.send_command("list".to_string()) {
+        Ok(resp) => { println!("{}", resp.body); }, // "Seed: [1871644822592853811]"
+        Err(err) => { println!("Command Error.\nErr: {:?}", err);},
+    }
+
     let bind_addr = "127.0.0.1:8080";
     let server = start_server(&bind_addr);
 
     println!();
-
     match server.await {
         Ok(_) => println!("Server terminated cleanly"),
         Err(err) => println!("Server terminated with an error!.\nErr: {:?}", err),
     }
+
+    // Disconnect cleanly when finished.
+    client.close().unwrap();
 
     Ok(())
 }
