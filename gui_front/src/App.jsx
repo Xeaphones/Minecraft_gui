f6ff061321a500c628f919fa1209cbbe69b6894b
YVelay <yohan.velay@ynov.com>
2024-07-16 23:22:18 +0200

    server properties

--- gui_front/src/App.jsx
+++ gui_front/src/App.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import Menu from './components/menu';
 import SideMenu from './components/side_menu';
+import ServerProperties from './components/ServerProperties';
 import './app.scss';
 
 function App() {
     return (
-        <div className="App">
-            <Menu />
-            <HomePage />
-            <SideMenu />
-        </div>
+        <Router>
+            <div className="App">
+                <Menu />
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/settings" element={<ServerProperties />} />
+                </Routes>
+                <SideMenu />
+            </div>
+        </Router>
     );
 }
 
